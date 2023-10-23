ed489e6ab6f542290e59da6e16c4ee5b6e389a6f
Denzel Hooke <denzelhooke@hotmail.com>
2023-10-22 19:55:10 -0700

    Fixed overflow issue

--- frontend/src/app/components/AddDocuments.tsx
+++ frontend/src/app/components/AddDocuments.tsx
@@ -12,37 +12,37 @@ interface Props {
 const AddDocuments = ({ setJobData, current, fileUrlState }: Props) => {
   return (
     <>
-      <div
-        className={`form-inputs ${
-          current.toLowerCase() !== 'documents' && 'hide'
-        }`}>
-        {fileUrlState && (
-          <iframe
-            src={`${fileUrlState.resumeUrl}`}
-            width="400px"
-            height="380px"></iframe>
-        )}
-
-        <div className="form-wrapper">
-          <label htmlFor="resume">Resume</label>
-          <input
-            id="resume"
-            className="input"
-            type="file"
-            // value={jobData.resume?.name}
-            onChange={(e) => {
-              // Grabbing input file
-              const files = (e.target as HTMLInputElement).files;
-
-              if (files && files.length > 0) {
-                const file = files[0];
-                setJobData((prevState: any) => {
-                  return { ...prevState, [e.target.id]: file };
-                });
-              }
-            }}
-            placeholder="Add File"
-          />
+      <div className="input__form__wrapper">
+        <div
+          className={`form-inputs ${
+            current.toLowerCase() !== 'documents' && 'hide'
+          }`}>
+          {fileUrlState && (
+            <iframe
+              src={`${fileUrlState.resumeUrl}`}
+              width="400px"
+              height="380px"></iframe>
+          )}
+          <div className="form-wrapper">
+            <label htmlFor="resume">Resume</label>
+            <input
+              id="resume"
+              className="input"
+              type="file"
+              // value={jobData.resume?.name}
+              onChange={(e) => {
+                // Grabbing input file
+                const files = (e.target as HTMLInputElement).files;
+                if (files && files.length > 0) {
+                  const file = files[0];
+                  setJobData((prevState: any) => {
+                    return { ...prevState, [e.target.id]: file };
+                  });
+                }
+              }}
+              placeholder="Add File"
+            />
+          </div>
         </div>
       </div>
     </>
--- frontend/src/app/components/CreateJobMainForm.tsx
+++ frontend/src/app/components/CreateJobMainForm.tsx
@@ -13,80 +13,82 @@ const CreateJobMainForm = ({ jobData, setJobData, current }: Props) => {
     <>
       <div
         className={`form-inputs ${current.toLowerCase() !== 'info' && 'hide'}`}>
-        <div className="form-wrapper">
-          <label htmlFor="title">Position</label>
-          <input
-            id="title"
-            className="input"
-            type="text"
-            value={jobData.title}
-            onChange={(e) =>
-              setJobData((prevState: CreateJobDto) => {
-                return { ...prevState, [e.target.id]: e.target.value };
-              })
-            }
-            placeholder="Enter title"
-          />
-        </div>
-        <div className="form-wrapper">
-          <label htmlFor="company">Company</label>
-          <input
-            id="company"
-            className="input"
-            type="text"
-            placeholder="Eg. Apple"
-            value={jobData.company}
-            onChange={(e) =>
-              setJobData((prevState: any) => {
-                return { ...prevState, [e.target.id]: e.target.value };
-              })
-            }
-          />
-        </div>
-        <div className="form-wrapper">
-          <label htmlFor="url">Page URL</label>
-          <input
-            id="url"
-            className="input"
-            type="text"
-            placeholder="indeed.com/job/123"
-            value={jobData.url}
-            onChange={(e) =>
-              setJobData((prevState: any) => {
-                return { ...prevState, [e.target.id]: e.target.value };
-              })
-            }
-          />
-        </div>
-        <div className="form-wrapper">
-          <label htmlFor="salary">Salary</label>
-          <input
-            id="salary"
-            className="input"
-            type="number"
-            placeholder="$37,000"
-            value={jobData.salary}
-            onChange={(e) =>
-              setJobData((prevState: any) => {
-                return { ...prevState, [e.target.id]: e.target.value };
-              })
-            }
-          />
-        </div>
-        <div className="form-wrapper">
-          <label htmlFor="location">Location</label>
-          <input
-            id="location"
-            className="input"
-            type="text"
-            placeholder="1455 Quebec St, Vancouver"
-            value={jobData.location}
-            onChange={(e) =>
-              setJobData((prevState: any) => {
-                return { ...prevState, [e.target.id]: e.target.value };
-              })
-            }
-          />
+        <div className="input__form__wrapper">
+          <div className="form-wrapper">
+            <label htmlFor="title">Position</label>
+            <input
+              id="title"
+              className="input"
+              type="text"
+              value={jobData.title}
+              onChange={(e) =>
+                setJobData((prevState: CreateJobDto) => {
+                  return { ...prevState, [e.target.id]: e.target.value };
+                })
+              }
+              placeholder="Enter title"
+            />
+          </div>
+          <div className="form-wrapper">
+            <label htmlFor="company">Company</label>
+            <input
+              id="company"
+              className="input"
+              type="text"
+              placeholder="Eg. Apple"
+              value={jobData.company}
+              onChange={(e) =>
+                setJobData((prevState: any) => {
+                  return { ...prevState, [e.target.id]: e.target.value };
+                })
+              }
+            />
+          </div>
+          <div className="form-wrapper">
+            <label htmlFor="url">Page URL</label>
+            <input
+              id="url"
+              className="input"
+              type="text"
+              placeholder="indeed.com/job/123"
+              value={jobData.url}
+              onChange={(e) =>
+                setJobData((prevState: any) => {
+                  return { ...prevState, [e.target.id]: e.target.value };
+                })
+              }
+            />
+          </div>
+          <div className="form-wrapper">
+            <label htmlFor="salary">Salary</label>
+            <input
+              id="salary"
+              className="input"
+              type="number"
+              placeholder="$37,000"
+              value={jobData.salary}
+              onChange={(e) =>
+                setJobData((prevState: any) => {
+                  return { ...prevState, [e.target.id]: e.target.value };
+                })
+              }
+            />
+          </div>
+          <div className="form-wrapper">
+            <label htmlFor="location">Location</label>
+            <input
+              id="location"
+              className="input"
+              type="text"
+              placeholder="1455 Quebec St, Vancouver"
+              value={jobData.location}
+              onChange={(e) =>
+                setJobData((prevState: any) => {
+                  return { ...prevState, [e.target.id]: e.target.value };
+                })
+              }
+            />
+          </div>
         </div>
       </div>
     </>
